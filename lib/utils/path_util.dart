d57dbd6bc7eb42c00d1b2be519ed2771f85f50c4
shangguanyanyan <1024503335@qq.com>
2020-07-17 09:00:05 +0800

    删除无用文件，修改依赖

---
+++ lib/utils/path_util.dart
@@ -0,0 +1,18 @@
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+class PathUtil{
+  static p.Context _context = p.Context();
+
+  PathUtil._();
+
+  /// 获取 应用程序的 data目录
+  ///
+  /// 如果在 runApp 之前调用，
+  /// 则需要先执行 `WidgetsFlutterBinding.ensureInitialized();`
+  static Future<String> getDataDir([String path]) async {
+    String dataDir = (await getApplicationDocumentsDirectory()).path;
+    dataDir = _context.join(dataDir, path);
+    return dataDir;
+  }
+
+}
